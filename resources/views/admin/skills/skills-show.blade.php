669aacd6ba62b76e53d9e947e07b63d351463f30
Abdelmoneim Belal <38110521+abdelmoneimbelal@users.noreply.github.com>
2024-12-23 23:24:53 +0200

    enhancing show modal

--- resources/views/admin/skills/skills-show.blade.php
+++ resources/views/admin/skills/skills-show.blade.php
@@ -1,31 +1,10 @@
-<!-- Modal -->
-<div class="modal fade" id="showModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Show skill</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col mb-0">
-                        <label class="form-label">Name</label>
-                        <input type="text" class="form-control" placeholder="Name" readonly wire:model='name' />
-                    </div>
-
-                    <div class="col mb-0">
-                        <label class="form-label">Progress</label>
-                        <input type="number" class="form-control" placeholder="10" min="1" max="100" readonly
-                            wire:model='progress' />
-                    </div>
-
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-secondary d-flex" data-bs-dismiss="modal">
-                    <i class="bx bx-x pe-1"></i> Close
-                </button>
-            </div>
-        </div>
+<x-show-modal title="Show Skill">
+    <div class="col-md-6 mb-0">
+        <label class="form-label">Name</label>
+        <div class="form-control"> {{ $name }}</div>
     </div>
-</div>
+    <div class="col-md-6 mb-0">
+        <label class="form-label">Progress</label>
+        <div class="form-control">{{ $progress }}</div>
+    </div>
+</x-show-modal>
